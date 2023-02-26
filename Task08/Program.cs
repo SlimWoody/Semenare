edd52d26337168e76fde8e00764cfee62dfb5512
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-26 18:29:41 +0400

    Added Homework Task 08

---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (number > 1)
+{
+    while (count <= number)
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
+}
+else
+{
+    Console.WriteLine("!!!Введите число больше 1!!!");
+}
