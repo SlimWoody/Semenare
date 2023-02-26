d43a7ecb551734e8bcaa10a8a8f01b259dc5381d
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-26 17:13:57 +0400

    Addad new folder error becaus of  open commit

---
+++ Task03.1/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine ("Введите число от 1 до 7: ");
+String day = Console.ReadLine ();
+switch (day)
+{
+    case "1":
+    {
+      Console.WriteLine ("Понедельник");
+      break;
+    }
+
+    case "2":
+    {
+      Console.WriteLine ("Вторник");
+      break;
+    }
+
+    case "3":
+    {
+      Console.WriteLine ("Среда");
+      break;
+    }
+
+    case "4":
+    {
+      Console.WriteLine ("Четверг");
+      break;
+    }
+
+    case "5":
+    {
+      Console.WriteLine ("Пятница");
+      break;
+    }
+
+    case "6":
+    {
+      Console.WriteLine ("Суббота");
+      break;
+    }
+
+    case "7":
+    {
+        Console.WriteLine ("Воскресенье");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine ("Неверные данные:");
+        Console.WriteLine ("<<Попробуйте снова>>");
+        break;
+    }
+}
