e92f345aa9297b4432247f147cce99bc42d28f3d
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-27 22:20:19 +0400

    Addad Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+int number  = new Random ().Next(100, 1000);
+int firstDigit = number/100;
+int secondDigit = number%10;
+int result = firstDigit * 10 + secondDigit;
+Console.WriteLine($"Случайное число отрезка {number} --> {result} ");
+
+int FirstDigit (int num)
+{
+  int firstDigit = number/100;
+  int secondDigit = number%10; 
+  int result = firstDigit * 10 + secondDigit ;
+  return result;
+}
+      
