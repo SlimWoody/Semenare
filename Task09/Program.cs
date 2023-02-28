d60c7dd7157a2037ecb7edf73f60f6f827851a55
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-28 23:15:46 +0400

    Addad Task 14

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,18 +1,18 @@
 ﻿int number  = new Random ().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
-int firstDigit = number/10;
-int secondDigit = number % 10;
+// int firstDigit = number/10;
+// int secondDigit = number % 10;
 
-if(firstDigit > secondDigit) 
-{
-    Console.WriteLine ($"Наибольшая цифра числа -> {firstDigit}");
-}
-else
-{
-    Console.WriteLine ($"Наибольшая цифра числа {secondDigit}");
-}
-int result = firstDigit > secondDigit? firstDigit : secondDigit;
-Console.WriteLine ($"Наибольшая цифра числа -> {result}");
+// if(firstDigit > secondDigit) 
+// {
+//     Console.WriteLine ($"Наибольшая цифра числа -> {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine ($"Наибольшая цифра числа {secondDigit}");
+// }
+// int result = firstDigit > secondDigit? firstDigit : secondDigit;
+// Console.WriteLine ($"Наибольшая цифра числа -> {result}");
 
 int maxDigit = MaxDigit (number);
 Console.WriteLine ($"Наибольшая цифра числа -> {maxDigit}");
--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,37 +1,18 @@
-﻿
-// 12. Напишите программу, которая будет принимать на
-// вход два числа и выводить, является ли первое число
-// кратным второму. Если число 1 не кратно числу 2, то
-// программа выводит остаток от деления.
-// 34, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
 
-Console.WriteLine ("Введите первое число: ");
-int number1 = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine ("Введите второе число: ");
-int number2 = Convert.ToInt32 (Console.ReadLine());
+int number = new Random().Next(100, 1000);
+int twoDigit = TwoDigit (number);
+Console.WriteLine($"Из трехзначного числа {number} получаем двухначное {twoDigit}");
 
-int result = number1%number2;
-if (result==0)
+int TwoDigit (int num)
 {
-    Console.WriteLine ("Кратное");
-}
-else
-{
-    Console.WriteLine ($"Не кратное, остаток {result}");
-}
-
-int Multiple (int num1,int num2)
-{
-  return num1%num2;   
-}
-
-int Multi;
-if (result==0)
-{
-    Console.WriteLine ("Кратное");
-}
-else
-{
-    Console.WriteLine ($"Не кратное, остаток {result}");
+int firstNumber = num / 100;
+int secondNumber = num % 10;
+int sum = firstNumber * 10 + secondNumber;
+return sum;
 }
