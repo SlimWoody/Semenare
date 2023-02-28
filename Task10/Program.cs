b3273dff8c14fc0ef822b75ffe172b458af47631
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-28 23:54:52 +0400

    Addad Homework Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// 10. Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трех значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int unambiguous = Unambiguous(number);
+Console.WriteLine($"Число: {unambiguous}");
+int Unambiguous (int num)
+{
+    int num1 = num / 100;
+    int num2 = num / 10;
+    int result = num2 - (num1 * 10);
+    return result;
+}
