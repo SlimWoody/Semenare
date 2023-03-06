4b2454d47578be4a310e0cfdca7c8e8f72f17826
Pavel_Luzin <st_dizainer@mail.ru>
2023-03-06 23:40:38 +0400

    Added Task 24, 26, 28, 29

---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int multiplication = Multiplication(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplication}");
+}
+else
+{
+    Console.WriteLine("Введите целое положительное число!");
+}
+
+int Multiplication(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            mult = mult * i;
+        }
+    }
+    return mult;
+}
