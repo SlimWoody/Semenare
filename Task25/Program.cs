bf218224126c7b1353f5ebbb7f4175ac08852520
Pavel_Luzin <st_dizainer@mail.ru>
2023-03-07 21:37:46 +0400

    Added HomeWork Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число 
+// A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите два положительных числа");
+Console.WriteLine("Первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int extentNumber = ExtentNumber(numberA, numberB);
+Console.WriteLine($"Число {numberA} натуральнтуральная степень {numberB} -> {extentNumber} ");
+
+int ExtentNumber(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        
+        result =result * num1;
+        
+    }
+    return result;
+}
+
