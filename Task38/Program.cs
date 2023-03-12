eb3f34cdc0783cb54592085a836a62fb5f3e6882
Pavel_Luzin <st_dizainer@mail.ru>
2023-03-12 22:47:22 +0400

    Added HomeWork Task 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,4 +1,15 @@
-﻿double[] CreateArrayRndDouble(int size, int min, int max)
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным 
+// и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2C
+
+double[] array = CreateArrayRndDouble(5, 1, 99);
+PrintArrayDouble(array);
+double diffMaxMinElement = DiffMaxMinElement(array);
+Console.WriteLine($"-> {diffMaxMinElement}");
+
+double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
@@ -10,6 +21,7 @@
     return arr;
 }
 
+
 void PrintArrayDouble(double[] arr)
 {
     Console.Write("[");
@@ -19,4 +31,21 @@ void PrintArrayDouble(double[] arr)
         else Console.Write($"{arr[i]}");
     }
     Console.WriteLine("]");
+}
+
+double DiffMaxMinElement(double[] arr)
+{
+
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        if (arr[i] < min) min = array[i];
+        if (arr[i] > max) max = array[i];
+
+    }
+    double diff = max - min;
+    diff = Math.Round(diff, 1);
+    return diff;
 }
